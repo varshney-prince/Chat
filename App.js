ae3085632602762b050af24d77392bc0bfa52f2b
pranav.kulshreshtha_cs.aiml19 <pranav.kulshreshtha_cs.aiml19>
2021-07-24 01:05:32 +0530

    made the welcome page, closes #2

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
 import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+
 import Constants from 'expo-constants';
+
 import LoginScreen from './components/LoginScreen';
 import SignUp from './components/SignUp';
+import Welcome from './components/Welcome';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 const Stack = createStackNavigator();
+
 export default function App() {
   return (
     <View style={styles.container}>
      <NavigationContainer>
        <Stack.Navigator screenOptions={{ headerShown: false }}>
+       <Stack.Screen name={'Welcome'} component={Welcome} />
         <Stack.Screen name={'LoginScreen'} component={LoginScreen} />
-          <Stack.Screen name={'SignUp'} component={SignUp} />
-  
+        <Stack.Screen name={'SignUp'} component={SignUp} />
       </Stack.Navigator>
      </NavigationContainer>
     
---
+++ components/Welcome.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { View, Text, StyleSheet, TextInput, Pressable, TouchableOpacity } from 'react-native';
+const LoginScreen=({ navigation }) =>{
+  const buttonRipple = {
+    color: 'purple',
+    borderless: true,
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.welcomeMsgContainer}>
+        <Text style={styles.welcomeText}>Welcome folks!</Text>
+        <Text style={styles.subtitle}>Have a seat! What would you like to do?</Text>
+      </View>
+    
+        <TouchableOpacity style={styles.button} >
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+        style={styles.button} android_ripple={buttonRipple}
+        onPress={() => navigation.navigate('SignUp')}>
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </TouchableOpacity>
+    </View>
+  );
+}
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center'
+  },
+  welcomeMsgContainer: {
+      //borderWidth: 1,
+      position: 'absolute',
+      top: 50,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginBottom: 15
+  },
+  welcomeText: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    margin: 10,
+    fontFamily: 'arial'
+  },
+  subtitle: {
+      marginBottom: 35,
+      fontSize: 15,
+      color: 'grey'
+  },
+  button: {
+    backgroundColor: 'white',
+    width: '70%',
+    borderWidth: 2,
+    height: 45,
+    borderRadius: 10,
+    marginBottom: '3%',
+    textAlign: 'center',
+  },
+  buttonText: {
+    textAlign: 'center',
+    flex: 1,
+    padding: 10,
+    fontFamily: 'arial',
+    color: '#595a5c'
+  },
+  buttonAlign: {
+    marginLeft: 20,
+    marginRight: 20,
+  },
+});
